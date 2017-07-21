cb6a6beb86057608dd4274eecdeccb9af2b21323
Robert Avery <daranable@gmail.com>
2017-07-21 00:50:34 -0700

    Add django messages snackbar messages.

--- views.py
+++ views.py
@@ -12,7 +12,10 @@ def register(request):
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully registered please log in')
-            return redirect(reverse('auth:login'))
+            redir = reverse('auth:login')
+            if 'reg_back' in request.session:
+                redir = request.session['reg_back']
+            return redirect(redir)
         elif 'err_back' in request.session:
             request.session['reg_form_err'] = request.POST
             return redirect(request.session['err_back'])
@@ -29,7 +32,6 @@ def login(request):
         if form.is_valid():
             user = User.objects.get(email=form.cleaned_data.get('email'))
             request.session['user'] = {'first_name': user.first_name, 'last_name': user.last_name, 'email': user.email, 'id': user.id}
-            print('logged in')
             return redirect(reverse('auth:success'))
         elif 'err_back' in request.session:
             request.session['login_form_err'] = request.POST
